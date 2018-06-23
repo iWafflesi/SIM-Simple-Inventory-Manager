5576ecb90ad82dcce39e3d6b52bd7d487e630f4c
Christopher Lloyd <waffles@christophersmbp.uconnect.utah.edu>
2018-06-23 09:06:58 -0600

    updating my branch

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,23 +1,6 @@
 import axios from "axios";
 
 export default {
-<<<<<<< HEAD
-	// Gets all books
-	getBooks: function() {
-		return axios.get("/api/books");
-	},
-	// Gets the book with the given id
-	getBook: function(id) {
-		return axios.get("/api/books/" + id);
-	},
-	// Deletes the book with the given id
-	deleteBook: function(id) {
-		return axios.delete("/api/books/" + id);
-	},
-	// Saves a book to the database
-	saveBook: function(bookData) {
-		return axios.post("/api/books", bookData);
-=======
 	// Gets all materials
 	getMaterials: function() {
 		return axios.get("/api/materials");
@@ -33,6 +16,5 @@ export default {
 	// Saves a book to the database
 	saveMaterial: function(bookData) {
 		return axios.post("/api/materials", bookData);
->>>>>>> master
 	}
 };
