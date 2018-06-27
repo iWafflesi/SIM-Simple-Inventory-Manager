ccb531089d03b01c1a67fe4310e65dbc321912bf
bwilliams1991 <bwilly91@msn.com>
2018-06-26 20:38:18 -0600

    clean Jobs, Inventory, Shipping files

--- client/src/components/Jobs/Jobs.js
+++ client/src/components/Jobs/Jobs.js
@@ -1,25 +1,11 @@
 import React from "react";
-import Router from "../Router";
+
 import Nav from "../Nav";
-import { Row, Col } from "../Grid";
-import NavBtn from "../NavButton";
-import { Link } from 'react-router-dom';
+
 
 const Jobs = () => (
 	<React.Fragment>
-			<Nav>
-				<Row>
-					<Col size='md-4'>
-				<NavBtn><Link to="/inventory/current">Inventory</Link></NavBtn>
-				</Col>
-				<Col size='md-4'>
-				<NavBtn><Link to="/jobs">Production</Link></NavBtn>
-				</Col>
-				<Col size='md-4'>
-				<NavBtn><Link to="/shipping">Shipping</Link></NavBtn>
-					</Col>
-				</Row>
-			</Nav>
+		<Nav/>
 			Jobs
 		</React.Fragment>
 );
