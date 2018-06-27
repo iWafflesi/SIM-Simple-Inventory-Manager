c5697d3ac1879207984f6417894b78e458669444
Christopher Lloyd <waffles@christophersmbp.uconnect.utah.edu>
2018-06-26 20:22:53 -0600

    added

---
+++ client/src/pages/CurrentInventory/CurrentInventory.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import DeleteBtn from "../../components/DeleteBtn";
+import Jumbotron from "../../components/Jumbotron";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components/Grid";
+import { List, ListItem } from "../../components/List";
+import { Input, TextArea, FormBtn } from "../../components/Form";
+import Table from "../../components/Table/Table";
