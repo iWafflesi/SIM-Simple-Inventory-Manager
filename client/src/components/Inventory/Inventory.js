813b835a152580926c64a8463582597d79be6945
Christopher Lloyd <waffles@christophersmbp.uconnect.utah.edu>
2018-06-28 19:38:47 -0600

    my content

--- client/src/components/Inventory/Inventory.js
+++ client/src/components/Inventory/Inventory.js
@@ -1,16 +1,35 @@
 import React from "react";
 import Nav from "../Nav";
+import { Row, Col, Container } from "../Grid";
+import "./Inventory.css"
+import Table from "../Table";
 import NavBtn from "../NavButton";
 import { Link } from 'react-router-dom'
+
 
 const Inventory = () => (
 	<React.Fragment>
 		<Nav/>
-		<NavBtn><Link to="/inventory/current">Current</Link></NavBtn>
-		<NavBtn><Link to="/inventory/history">History</Link></NavBtn>
-		<NavBtn><Link to="/inventory/receiving">Receiving</Link></NavBtn>
+		<Container fluid>
+				<Row>
+					<Col size="md-12">
+					<Row>
+					<Col size="md-4">
+						<NavBtn><Link to="/inventory/current">Current</Link></NavBtn>
+						</Col>
+						<Col size="md-4">
+						<NavBtn><Link to="/inventory/history">History</Link></NavBtn>
+						</Col>
+						<Col size="md-4">
+						<NavBtn><Link to="/inventory/receiving">Receiving</Link></NavBtn>
+						</Col>
+						</Row>
+					</Col>
+				</Row>
+			<Table/>
+		</Container>
 			Inventory.current
-		</React.Fragment>
+	</React.Fragment>
 );
 
 export default Inventory;
