08d891d2f8a08c550d585b9c65b78ac05c6c39b9
Christopher Lloyd <waffles@christophersmbp.uconnect.utah.edu>
2018-07-12 19:34:07 -0600

    major overhaul

--- client/src/pages/Shipping/shipping.js
+++ client/src/pages/Shipping/shipping.js
@@ -1,127 +1,48 @@
 import React, { Component } from "react";
-import DeleteBtn from "../../components/DeleteBtn";
-import API from "../../utils/API";
-import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
-import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/Form";
-import Table from "../../components/Table/Table";
-import Nav from "../../components/Nav";
-import NavButton from "../../components/NavButton";
+import PartTable from "../../components/PartTable";
 
-class Parts extends Component {
-	state = {
-		parts: [],
-		name: "",
-		sku: "",
-		quantity: "",
-		material: "",
-	};
 
-	componentDidMount() {
-		this.loadParts();
-	}
 
-	loadParts = () => {
-		API.getParts()
-			.then(res =>
-				this.setState({ parts: res.data, name: "", sku: "", quantity: "",material: "" })
-			)
-			.catch(err => console.log(err));
-	};
+class Shipping extends Component {
 
-	deletePart = id => {
-		API.deletePart(id)
-			.then(res => this.loadParts())
-			.catch(err => console.log(err));
-	};
-
-	handleInputChange = event => {
-		const { name, value } = event.target;
-		this.setState({
-			[name]: value
-		});
-	};
-
-	handleFormSubmit = event => {
-		event.preventDefault();
-		if (this.state.name && this.state.sku) {
-			API.savePart({
-				name: this.state.name,
-				sku: this.state.sku,
-				quantity: this.state.quantity,
-				material: this.state.material
-			})
-			.then(res => this.loadParts())
-			.catch(err => console.log(err));
-		}
-	};
 	
 	render() {
-		return (
-			<Container fluid>
-			<Nav />
-				<Row>
-					<Col size="md-12">
 
-					<Table>
-						<form>
-							<Input
-								value={this.state.name}
-								onChange={this.handleInputChange}
-								name="name"
-								placeholder="name (Optional)"
-								/>
-							<Input
-								value={this.state.sku}
-								onChange={this.handleInputChange}
-								name="sku"
-								placeholder="sku (required)"
-								/>
-							<Input
-								value={this.state.quantity}
-								onChange={this.handleInputChange}
-								name="quantity"
-								placeholder="quantity (required)"
-								/>
-								<Input
-								value={this.state.qmaterial}
-								onChange={this.handleInputChange}
-								name="material"
-								placeholder="material used (Optional)"
-								/>
-							<FormBtn
-								disabled={!(this.state.sku && this.state.quantity)}
-								onClick={this.handleFormSubmit}
-								>
-								Submit Part
-							</FormBtn>
-						</form>
-						</Table>
-					</Col>
+		return (
+			<React.Fragment>
+			<Container fluid>
+
+			<div className="card">
+			<div className="card-body">
+				<Row>
 					<Col size="md-6 sm-12">
 
-						{this.state.parts.length ? (
-							<List>
-								{this.state.parts.map(part => (
-									<ListItem key={part._id}>
-										<Link to={"/parts/" + part._id}>
-											<strong>
-												{part.name} by {part.sku}
-											</strong>
-										</Link>
-										<DeleteBtn onClick={() => this.deletePart(part._id)} />
-									</ListItem>
-								))}
-							</List>
-						) : (
-							<h3>No Results to Display</h3>
-						)}
+	<div className="panel panel-default">
+				<div className="panel heading text-center"><h3>Current Jobs</h3></div>
+				<div className="panel-body">
+					<table className="table table-hover" id='JobsTable'>
+						<thead>
+							<tr>
+								<th scope="col">Name</th>
+								<th scope="col">Part Sku</th>
+								<th scope="col">quantity</th>
+							</tr>
+						</thead>
+						<tbody>
+							<PartTable />
+						</tbody>
+					</table>
+				</div>
+			</div>
 					</Col>
 				</Row>
+			</div>
+			</div>
 			</Container>
+			</React.Fragment>
 		);
-	}
+	};
 }
 
-export default Books;
+export default Shipping;
