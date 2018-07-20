c4b6ffa2865f2b2650b1914d8e76d7fec29af2d5
Christopher Lloyd <waffles@Christophers-MacBook-Pro.local>
2018-07-19 20:33:30 -0600

    stuff

--- service/passport.js
+++ service/passport.js
@@ -7,7 +7,7 @@ const User = mongoose.model("User");
 // Tell passport to make user of LocalStrategy
 passport.use(new LocalStrategy((username, password, done) => {
 	console.log(username);
-  User.findOne({ username: username }, (err, user) => {
+  User.findOne({ username: username, password: password }, (err, user) => {
     // if (err) return done(err);
     // if (!user) return done(null, false);
     // if (user.password !== password) return done(null, false);
